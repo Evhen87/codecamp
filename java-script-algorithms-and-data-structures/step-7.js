b33897fe39a5f206b25a0327a001867e10d37751
Evhen87 <kushner.aleks@yandex.ru>
2025-03-14 23:12:56 +0300

    step 7 (JS)

---
+++ java-script-algorithms-and-data-structures/step-7.js
@@ -0,0 +1,8 @@
+//Step 7
+//Now log your character variable to the console again. You should see the 
+//string "Hello", then the string "World", in the console.
+//Solution
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
