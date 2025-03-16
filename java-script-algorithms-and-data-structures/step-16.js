f4f7d443dd46ff57041fe709b2e29048a926b4d0
Evhen87 <kushner.aleks@yandex.ru>
2025-03-16 23:11:40 +0300

    Step 16

---
+++ java-script-algorithms-and-data-structures/step-16.js
@@ -0,0 +1,6 @@
+//Step 16
+//With the number data type, you can perform mathematical operations, like addition. Try printing count + 1 to the console.
+//Solution
+let character = 'Hello';
+let count = 8;
+console.log(count + 1);
