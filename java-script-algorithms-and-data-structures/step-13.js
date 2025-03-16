d13403b612190654b328c53231d4f3f86a468806
Evhen87 <kushner.aleks@yandex.ru>
2025-03-16 23:03:40 +0300

    Step 13

--- java-script-algorithms-and-data-structures/step-13.js
+++ java-script-algorithms-and-data-structures/step-13.js
@@ -4,3 +4,9 @@
 //Use the let keyword to declare a profession variable and an age variable. Initialize profession with the string "teacher", but do not initialize age with any value.
 
 //Log both of your variables to the console to see the results.
+//Solution
+let character = "Hello";
+let profession = "teacher";
+let age;
+console.log(profession);
+console.log(age);
