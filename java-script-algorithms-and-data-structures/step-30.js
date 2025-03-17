103c464bbb8820de371275e78a52431454afd3ee
Evhen87 <kushner.aleks@yandex.ru>
2025-03-17 21:30:51 +0300

    Step 30

---
+++ java-script-algorithms-and-data-structures/step-30.js
@@ -0,0 +1,8 @@
+//Step 30
+//Change your rows declaration to be assigned an empty array again.
+
+//Also, change your 'Hello' string to use double quotes again. Generally, it does not matter which of the two you prefer, but you will want to be consistent in that choice throughout your project.
+//Solution
+let character = "Hello";
+let count = 8;
+let rows = [];
