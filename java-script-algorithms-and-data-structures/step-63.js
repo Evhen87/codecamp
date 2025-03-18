7f11e1ff098e486e7babdc407da90485cdcf070a
Evhen87 <kushner.aleks@yandex.ru>
2025-03-18 23:19:49 +0300

    Step 63

---
+++ java-script-algorithms-and-data-structures/step-63.js
@@ -0,0 +1,23 @@
+//Step 63
+//As expected, your function now returns undefined again. Your call variable is not necessary any more, so remove the call declaration and the console.log for the call variable.
+//Solution
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+
+}
+
+
+for (let i = 0; i < count; i = i + 1) {
+   rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+   result = result + row + "\n";
+}
+
+console.log(result);
