94b0d9899cc966fa3e2b3f97870efe3d25a88e1c
Evhen87 <kushner.aleks@yandex.ru>
2025-03-19 20:58:43 +0300

    Step 84

---
+++ java-script-algorithms-and-data-structures/step-84.js
@@ -0,0 +1,28 @@
+//Step 84
+//Now that you have practiced working with if...else if...else statements, you can remove them from your code.
+
+//Once you complete that, use let to declare a continueLoop variable and assign it the boolean false. Then use let to declare a done variable and assign it the value 0.
+//Solution
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+let continueLoop = false;
+let done = 0;
+
+let result = ""
+
+for (const row of rows) {
+   result = result + row + "\n";
+}
+
+console.log(result);
