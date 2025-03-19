4d2866cdc1bf302ceea1549fa163bbb9edce7cbd
Evhen87 <kushner.aleks@yandex.ru>
2025-03-19 21:51:06 +0300

    Step 93

---
+++ java-script-algorithms-and-data-structures/step-93.js
@@ -0,0 +1,34 @@
+
+//Step 93
+//Your loop is no longer relying on the continueLoop variable. This makes the variable an unused declaration. Generally, you want to avoid unused declarations to prevent future confusion.
+
+//Remove your continueLoop variable.
+//Solution
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+
+let done = 0;
+
+while (done !== count) {
+   done++;
+   rows.push(padRow(done, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+   result = result + row + "\n";
+}
+
+console.log(result);
