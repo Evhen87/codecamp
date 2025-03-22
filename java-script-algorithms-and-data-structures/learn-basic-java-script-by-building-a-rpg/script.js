00c9fb66d91e27b6ccfca2f5f88b21d81ff4bc28
Evhen87 <kushner.aleks@yandex.ru>
2025-03-23 00:48:45 +0300

    Step 9

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -18,7 +18,10 @@
 Initialize another variable called health with a value of 100, and a variable called gold with a value of 50.*/
 /*Step 8
 Create another variable called currentWeaponIndex and set it to 0.*/
+/*Step 9
+Declare a variable called fighting but do not initialize it with a value.*/
 let xp = 0;
 let health = 100;
 let gold = 50;
 let currentWeaponIndex = 0;
+let fighting;
