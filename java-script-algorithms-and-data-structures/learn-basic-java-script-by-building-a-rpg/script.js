e580057a340a75295baaef42e6ca9582763e1875
Evhen87 <kushner.aleks@yandex.ru>
2025-03-23 23:39:41 +0300

    Step 46

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -141,6 +141,21 @@ You need to wrap the text Store in double quotes. Because your string is already
 Example Code
 const escapedString = "Naomi likes to play \"Zelda\" sometimes.";
 Wrap the text Store in double quotes within your text.innerText line.*/
+/*Step 46
+You have repetition in the goTown and goStore functions. Repetition in your code is a sign that you need another function.
+
+In the previous project, you learned how to work with function parameters like this:
+
+Example Code
+function myFunction(param) {
+  console.log(param);
+}
+Function parameters act as placeholders for values that you pass to the function when you call it.
+
+Create an empty update function that takes a parameter called location.*/
+function update(location) {
+
+}
 function goTown() {
    button1.innerText = "Go to store";
    button2.innerText = "Go to cave";
