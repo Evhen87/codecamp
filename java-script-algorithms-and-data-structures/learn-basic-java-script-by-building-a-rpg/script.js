3f3a970c5cf113d95b18e4760d7410a98abce378
Evhen87 <kushner.aleks@yandex.ru>
2025-03-23 23:58:45 +0300

    Step 51

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -161,7 +161,26 @@ const cat = {
    name: "Whiskers",
    "Number of legs": 4
 };
-console.log(cat);
+/*Step 51
+There are two ways to access the properties of an object: dot notation (.) and bracket notation ([]), similar to an array.
+
+Dot notation is what you use when you know the name of the property you're trying to access ahead of time.
+
+Example Code
+object.property;
+Here is a sample of using dot notation (.) to read the name property of the developer object:
+
+Example Code
+const developer = {
+  name: "Jessica",
+}
+
+// Output: Jessica
+console.log(developer.name); 
+Update your console statement to access the name property of the cat object using dot notation.
+
+Open up the console to see the name of "Whiskers" logged to the console.*/
+console.log(cat.name);
 //initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
