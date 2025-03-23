617e190c6f991e171215d5c6f7a6709135e4229c
Evhen87 <kushner.aleks@yandex.ru>
2025-03-23 22:40:12 +0300

    Step 36

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -88,7 +88,18 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 //// I am a single-line comment
 //  I am a multi-line comment
 //Add a single-line comment that says initialize buttons.*/
+/*Step 36
+button1 represents your first button element. These elements have a special property called onclick, which you can use to determine what happens when someone clicks that button.
+
+You can access properties in JavaScript a couple of different ways. The first is with dot notation. Here is an example of using dot notation to set the onclick property of a button to a function reference.
+
+Example Code
+button.onclick = myFunction;
+In this example, button is the button element, and myFunction is a reference to a function. When the button is clicked, myFunction will be called.
+
+Use dot notation to set the onclick property of your button1 to the function reference of goStore. Note that button1 is already declared, so you don't need to use let or const.*/
 //initialize buttons
+button1.onclick = goStore;
 function goStore() {
    console.log("Going to store.")
 }
