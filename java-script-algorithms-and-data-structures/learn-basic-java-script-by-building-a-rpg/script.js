f2a324cf50d20c94382c64a65a7fba51f152f818
Evhen87 <kushner.aleks@yandex.ru>
2025-03-23 20:53:04 +0300

    Step 17

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -46,4 +46,8 @@ let monsterHealth;
 //Note that h1 is a string and matches the CSS selector you would use.
 
 //Create a button1 variable and use querySelector() to assign it your element with the id of button1. Remember that CSS id selectors are prefixed with a #.*/
-let button1 = document.querySelector("#button1");
+/*Step 17
+//button1 is a variable that is not going to be reassigned. If you are not going to assign a new value to a variable, it is best practice to use the const keyword to declare it instead of the let keyword. This will tell JavaScript to throw an error if you accidentally reassign it.
+
+//Change your button1 variable to be declared with the const keyword.*/
+const button1 = document.querySelector("#button1");
