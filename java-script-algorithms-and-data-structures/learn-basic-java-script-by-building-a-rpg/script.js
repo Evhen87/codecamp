e87b96ffb629262491bf3387bea988edbfab4c6c
Evhen87 <kushner.aleks@yandex.ru>
2025-03-23 22:51:56 +0300

    Step 40

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -119,10 +119,21 @@ The example above would change the text of the p element from Demo content to He
 When a player clicks your Go to store button, you want to change the buttons and text. Remove the code inside the goStore function and add a line that updates the text of button1 to say "Buy 10 health (10 gold)".*/
 /*Step 39
 //Now, add a line that updates the text of button2 to say "Buy weapon (30 gold)" and update the text of button3 to say "Go to town square".*/
+/*Step 40
+You will also need to update the functions that run when the buttons are clicked again.
+
+In your goStore() function, update the onclick property for each button to run buyHealth, buyWeapon, and goTown, respectively.*/
+/*Step 40
+You will also need to update the functions that run when the buttons are clicked again.
+
+In your goStore() function, update the onclick property for each button to run buyHealth, buyWeapon, and goTown, respectively.*/
 function goStore() {
    button1.innerText = "Buy 10 health (10 gold)";
    button2.innerText = "Buy weapon (30 gold)";
    button3.innerText = "Go to town square";
+   button1.onclick = buyHealth;
+   button2.onclick = buyWeapon;
+   button3.onclick = goTown;
 }
 /*Step 33
 //Now create a goCave function that prints "Going to cave." to the console.*/
