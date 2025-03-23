903e71db1835a626098d2a410e98b0502bae641b
Evhen87 <kushner.aleks@yandex.ru>
2025-03-23 22:45:30 +0300

    Step 38

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -106,8 +106,19 @@ Once you have done that, open your console and try clicking the buttons on your 
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+/*Step 38
+The innerText property controls the text that appears in an HTML element. For example:
+
+Example Code
+<p id="info">Demo content</p> 
+Example Code
+const info = document.querySelector("#info"); 
+info.innerText = "Hello World"; 
+The example above would change the text of the p element from Demo content to Hello World.
+
+When a player clicks your Go to store button, you want to change the buttons and text. Remove the code inside the goStore function and add a line that updates the text of button1 to say "Buy 10 health (10 gold)".*/
 function goStore() {
-   console.log("Going to store.")
+   button1.innerText = "Buy 10 health (10 gold)";
 }
 /*Step 33
 //Now create a goCave function that prints "Going to cave." to the console.*/
