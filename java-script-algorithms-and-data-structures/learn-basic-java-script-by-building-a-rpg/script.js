cc76b03c8386b025612f7320144af9d0e21ad777
Evhen87 <kushner.aleks@yandex.ru>
2025-03-23 23:56:58 +0300

    Step 50

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -137,8 +137,29 @@ const obj = {
 Inside your cat object, add a new property. The key should be name and the value should be the string "Whiskers".
 
 Open up the console to see the updates to your object.*/
+/*Step 50
+If the property name (key) of an object has a space in it, you will need to use single or double quotes around the name.
+
+Here is an example of an object with a property name that has a space:
+
+Example Code
+const spaceObj = {
+  "Space Name": "Kirk",
+};
+If you tried to write a key without the quotes, it would throw an error:
+
+Example Code
+const spaceObj = {
+  // Throws an error
+  Space Name: "Kirk",
+}; 
+
+Add a new property with a key of "Number of legs" and value of 4 to the cat object.
+
+Open up the console to see the output.*/
 const cat = {
-   name: "Whiskers"
+   name: "Whiskers",
+   "Number of legs": 4
 };
 console.log(cat);
 //initialize buttons
