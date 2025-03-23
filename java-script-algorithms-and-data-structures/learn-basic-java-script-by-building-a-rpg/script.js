30eecab3e35ee8a6970f0e13ef8cdcd429fad3b5
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 00:01:08 +0300

    Step 52

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -180,7 +180,23 @@ console.log(developer.name);
 Update your console statement to access the name property of the cat object using dot notation.
 
 Open up the console to see the name of "Whiskers" logged to the console.*/
-console.log(cat.name);
+/*Step 52
+The second way to access the properties of an object is bracket notation ([]). If the property of the object you are trying to access has a space in its name, you will need to use bracket notation.
+
+Example Code
+objectName["property name"];
+Here is a sample of using bracket notation to read an object's property:
+
+Example Code
+const spaceObj = {
+  "Space Name": "Kirk",
+};
+
+spaceObj["Space Name"]; // "Kirk"
+Update your console statement to use bracket notation to access the property "Number of legs" of the cat object.
+
+Open up the console to see the output.*/
+console.log(cat["Number of legs"]);
 //initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
