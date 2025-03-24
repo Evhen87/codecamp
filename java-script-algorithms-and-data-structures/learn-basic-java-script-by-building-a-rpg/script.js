8b0862bda64af58066b1f323f965cc502d087aca
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 20:39:49 +0300

    Step 78

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -405,12 +405,25 @@ if (num >= 3) {
   console.log("This code will run because num is greater than or equal to 3.");
 }
 Start by placing all of the code in your buyHealth function inside an if statement. For the if statement condition, check if gold is greater than or equal to 10.*/
+/*Step 78
+Now when a player tries to buy health, it will only work if they have enough money. If they do not, nothing will happen. Add an else statement where you can put code to run if a player does not have enough money.
+
+In the previous project, you learned how to work with else statements like this:
+
+Example Code
+if (num >= 5) {
+
+} else {
+
+}*/
 function buyHealth() {
    if (gold >= 10) {
       gold -= 10;
       health += 10;
       goldText.innerText = gold;
       healthText.innerText = health;
+   } else {
+
    }
 }
 function buyWeapon() {
