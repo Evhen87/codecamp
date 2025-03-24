9f8bbe9d128b720d579b9bbacdb6c1fe833b4ed2
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 19:47:27 +0300

    Step 73

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -376,7 +376,12 @@ function fightDragon() {
 }
 /*Step 42
 Create three new empty functions called buyHealth, buyWeapon, and goTown.*/
+/*Step 73
+Now that your "store" and "cave" locations are complete, you can code the actions the player takes at those locations. Inside the buyHealth function, set gold equal to gold minus 10.
+
+For example, here is how you would set num equal to 5 less than num: num = num - 5;.*/
 function buyHealth() {
+   gold = gold - 10;
 }
 function buyWeapon() {
 }
