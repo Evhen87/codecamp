29105b1c61a62e237368db44db922728ae28fbb7
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 23:12:16 +0300

    Step 110

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -169,6 +169,15 @@ const weapons = [
       power: 100
    }
 ];
+/*Step 110
+Now you can start the code to fight monsters. To keep your code organized, your fightDragon function has been moved for you to be near the other fight functions.
+
+Below your weapons array, define a monsters variable and assign it an array. Set that array to have three objects, each with a name, level, and health properties. The first object's values should be "slime", 2, and 15, in order. The second should be "fanged beast", 8, and 60. The third should be "dragon", 20, and 300.*/
+const monsters = [
+   { name: 'slime', level: 2, health: 15 },
+   { name: 'fanged beast', level: 8, health: 60 },
+   { name: 'dragon', level: 20, health: 300 }
+];
 const locations = [
    {
       name: "town square",
