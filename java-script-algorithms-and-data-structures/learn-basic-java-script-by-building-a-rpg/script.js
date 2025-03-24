98f490ff3ab9b797ff49876eccb5c3e3edfee8f4
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 21:34:18 +0300

    Step 86

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -467,10 +467,14 @@ num++;
 // prints 6
 console.log(num); 
 Change your currentWeaponIndex assignment to use the increment operator.*/
+/*Step 86
+Now update the goldText element to display the new value of gold, and update the text element to display "You now have a new weapon.".*/
 function buyWeapon() {
    if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
+      goldText.innerText = gold;
+      text.innerText = "You now have a new weapon.";
    }
 }
 /*Step 70
