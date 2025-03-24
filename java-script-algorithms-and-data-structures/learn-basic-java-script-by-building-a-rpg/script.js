6a21b716a79ef5f3d3cf65758548e66c88b5637c
Evhen87 <kushner.aleks@yandex.ru>
2025-03-25 00:06:36 +0300

    Step 118

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -649,9 +649,22 @@ Fighting each type of monster will use similar logic. Create an empty function c
 In the goFight function, call your update function with the fourth object in locations as an argument.*/
 /*Step 117
 Below your update call, set the monsterHealth to be the health of the current monster. You can get this value by accessing the health property of monsters[fighting] with dot notation.*/
+/*Step 118
+By default, the HTML element that shows the monster's stats has been hidden with CSS. When the player clicks the "Fight dragon" button, the monster's stats should be displayed. You can accomplish this by using the style and display properties on the monsterStats element.
+
+The style property is used to access the inline style of an element and the display property is used to set the visibility of an element.
+
+Here is an example of how to update the display for a paragraph element:
+
+Example Code
+const paragraph = document.querySelector('p');
+paragraph.style.display = 'block';
+Display the monsterStats element by updating the display property of the style property to block.*/
 function goFight() {
    update(locations[3]);
-   monsterHealth = monsters[fighting].health
+   monsterHealth = monsters[fighting].health;
+   const monsterStats = document.querySelector('#monsterStats ');
+   monsterStats.style.display = 'block';
 }
 /*Step 114
 At the end of your code, create two empty functions named attack and dodge.*/
