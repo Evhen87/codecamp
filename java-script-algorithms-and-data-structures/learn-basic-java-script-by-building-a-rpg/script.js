a95ad883c88bd49c28955200f0ed9f882c36c028
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 21:42:05 +0300

    Step 90

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -482,13 +482,22 @@ Example Code
 let value = array[index];*/
 /*Step 89
 weapons[currentWeaponIndex] is an object. Use dot notation to get the name property of that object.*/
+/*Step 90
+In the previous project, you learned how to work with the concatenation operator to insert variables into a string like this:
+
+Example Code
+const organization = "freeCodeCamp";
+
+// "Hello, our name is freeCodeCamp."
+"Hello, our name is " + organization + ".";
+Update the string "You now have a new weapon." to "You now have a " followed by the name of the new weapon, and remember to end the sentence with a period.*/
 function buyWeapon() {
    if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
       goldText.innerText = gold;
       let newWeapon = weapons[currentWeaponIndex].name;
-      text.innerText = "You now have a new weapon.";
+      text.innerText = "You now have a " + newWeapon + ".";
    }
 }
 /*Step 70
