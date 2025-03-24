4ec999be8eccbc87fd35f108dbd951076a78a4ed
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 21:26:29 +0300

    Step 82

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -450,7 +450,10 @@ function buyHealth() {
       text.innerText = "You do not have enough gold to buy health."
    }
 }
+/*Step 82
+Inside your buyWeapon function, add an if statement to check if gold is greater than or equal to 30.*/
 function buyWeapon() {
+   if (gold >= 30) { }
 }
 /*Step 70
 Create two more empty functions named fightSlime and fightBeast. These functions will be used in your upcoming cave object.*/
