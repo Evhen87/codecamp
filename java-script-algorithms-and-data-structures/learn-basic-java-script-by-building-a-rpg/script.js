7bea19f64c3698fcd3541bf332e3b078a9cdb136
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 23:07:13 +0300

    Step 108

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -590,12 +590,15 @@ const firstElement = myArray.shift();
 Use the shift() method to take the first element from the inventory array and assign it to your currentWeapon variable.*/
 /*Step 107
 After your currentWeapon, use the concatenation operator to set text.innerText to the string "You sold a ", then currentWeapon, then the string ".".*/
+/*Step 108
+Now use the += operator to add the string " In your inventory you have: " and the contents of inventory to the text.innerText. Make sure to include the space at the beginning and end of the " In your inventory you have: " string.*/
 function sellWeapon() {
    if (inventory.length > 1) {
       gold += 15;
       goldText.innerText = gold;
       let currentWeapon = inventory.shift();
       text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
    }
 }
 /*Step 70
