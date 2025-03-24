4965d56a3e388d348b84bb0306bfce01327b755f
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 21:50:36 +0300

    Step 93

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -504,6 +504,8 @@ myArray.push("new item");
 Up until now, any time text.innerText was updated, the old text was erased. This time, use the += operator to add text to the end of text.innerText.
 
 Add the string " In your inventory you have: " - include the spaces at the beginning and the end.*/
+/*Step 93
+At the end of the second text.innerText string you just added, use the concatenation operator to add the contents of inventory to the string.*/
 function buyWeapon() {
    if (gold >= 30) {
       gold -= 30;
@@ -512,7 +514,7 @@ function buyWeapon() {
       let newWeapon = weapons[currentWeaponIndex].name;
       text.innerText = "You now have a " + newWeapon + ".";
       inventory.push(newWeapon);
-      text.innerText += " In your inventory you have: "
+      text.innerText += " In your inventory you have: " + inventory;
    }
 }
 /*Step 70
