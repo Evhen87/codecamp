64046dccf5dbbcaf390e389d1815df80a3462099
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 19:19:44 +0300

    Step 61

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -302,8 +302,15 @@ function update(location) {
    button3.onclick = fightDragon;
    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
-function goTown() {
+/*Step 61
+Instead of assigning the innerText and onclick properties to specific strings and functions, the update function will use data from the location that is passed into it. First, that data needs to be passed.
 
+Inside the goTown function, call the update function. Here is an example of calling a function named myFunction:
+
+Example Code
+myFunction();*/
+function goTown() {
+   update();
 }
 function goStore() {
 
