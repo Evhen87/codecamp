63146aa052ef601f436aaf410e7c28a6af46c1be
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 19:40:57 +0300

    Step 70

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -366,3 +366,11 @@ function buyHealth() {
 }
 function buyWeapon() {
 }
+/*Step 70
+Create two more empty functions named fightSlime and fightBeast. These functions will be used in your upcoming cave object.*/
+function fightSlime() {
+
+}
+function fightBeast() {
+
+}
