2cf8a3a660f0435fd1279e42498aeb8dc7346d55
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 22:59:30 +0300

    Step 105

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -569,10 +569,22 @@ Create an empty sellWeapon function.*/
 Players should not be able to sell their only weapon. Inside the sellWeapon function, add an if statement with a condition that checks if the length of the inventory array is greater than 1.*/
 /*Step 104
 Inside the if statement, set gold equal to 15 more than its current value. Also update goldText.innerText to the new value.*/
+/*Step 105
+The next step is to create a variable called currentWeapon.
+
+Example Code
+let num = 1;
+if (num === 1) {
+  let num = 2; // this num is scoped to the if statement
+  console.log(num); // expected output: 2
+}
+console.log(num); // expected output: 1 (the global variable)
+Use the let keyword to create a variable named currentWeapon. Don't assign it a value yet.*/
 function sellWeapon() {
    if (inventory.length > 1) {
       gold += 15;
       goldText.innerText = gold;
+      let currentWeapon;
    }
 }
 /*Step 70
