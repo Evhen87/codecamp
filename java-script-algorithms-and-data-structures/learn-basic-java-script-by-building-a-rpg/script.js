6be5d13f658d76ed704a6810e0af25fed70c4053
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 19:23:09 +0300

    Step 62

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -309,8 +309,16 @@ Inside the goTown function, call the update function. Here is an example of call
 
 Example Code
 myFunction();*/
+/*Step 62
+Now it is time to use your update function. Pass in your locations array into the update function call.
+
+You pass arguments by including them within the parentheses of the function call. For example, calling myFunction with an arg argument would look like:
+
+Example Code
+myFunction(arg)
+Pass your locations array into the update call.*/
 function goTown() {
-   update();
+   update(locations);
 }
 function goStore() {
 
