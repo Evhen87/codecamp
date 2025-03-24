c5319f891c0721bc13c11c215dbdb72bafb83efa
Evhen87 <kushner.aleks@yandex.ru>
2025-03-24 21:38:41 +0300

    Step 89

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -480,12 +480,14 @@ For example, this code uses the index variable to access a value of array.
 
 Example Code
 let value = array[index];*/
+/*Step 89
+weapons[currentWeaponIndex] is an object. Use dot notation to get the name property of that object.*/
 function buyWeapon() {
    if (gold >= 30) {
       gold -= 30;
       currentWeaponIndex++;
       goldText.innerText = gold;
-      let newWeapon = weapons[currentWeaponIndex];
+      let newWeapon = weapons[currentWeaponIndex].name;
       text.innerText = "You now have a new weapon.";
    }
 }
