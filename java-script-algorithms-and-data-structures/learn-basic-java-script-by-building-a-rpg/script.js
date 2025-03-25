da4ab0bec80220a72d2cd38dbbd5aee562e8217b
Evhen87 <kushner.aleks@yandex.ru>
2025-03-25 22:18:00 +0300

    Step 146

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -782,8 +782,12 @@ function attack() {
 Inside the dodge function, set text.innerText equal to the string "You dodge the attack from the <monster>". Replace <monster> with the name of the monster, using the name property.*/
 /*Step 145
 Below your attack function, create an empty function named getMonsterAttackValue. It should take level as a parameter.*/
-function getMonsterAttackValue(level) {
+/*Step 146
+The attack of the monster will be based on the monster's level and the player's xp. In the getMonsterAttackValue function, use const to create a variable called hit. Assign it the equation (level * 5) - (Math.floor(Math.random() * xp));.
 
+This will set the monster's attack to five times their level minus a random number between 0 and the player's xp.*/
+function getMonsterAttackValue(level) {
+   const hit = (level * 5) - (Math.floor(Math.random() * xp));
 }
 function dodge() {
    text.innerText = "You dodge the attack from the " + monsters[fighting].name
