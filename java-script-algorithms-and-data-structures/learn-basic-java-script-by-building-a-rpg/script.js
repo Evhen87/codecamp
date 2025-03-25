0d22e0be9a81f559629cb9d50b456e654696b373
Evhen87 <kushner.aleks@yandex.ru>
2025-03-25 23:09:22 +0300

    Step 157

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -776,6 +776,17 @@ let num = 10;
 num--;
 console.log(num); // Output: 9
 Decrement the value of currentWeaponIndex in your if statement, after you update the text.*/
+/*Step 157
+We don't want a player's only weapon to break. The logical AND operator checks if two statements are true.
+
+Use the logical AND operator && to add a second condition to your if statement. The player's weapon should only break if inventory.length does not equal (!==) one.
+
+Here is an example of an if statement with two conditions:
+
+Example Code
+if (firstName === "Quincy" && lastName === "Larson") {
+
+}*/
 function attack() {
    text.innerText = "The " + monsters[fighting].name + " attacks.";
    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
@@ -796,8 +807,8 @@ function attack() {
          defeatMonster();
       }
    }
-   if (Math.random() <= .1) {
-      text.innerText += " Your " + inventory.pop() + " breaks."
+   if (Math.random() <= .1 && inventory.length !== 1) {
+      text.innerText += " Your " + inventory.pop() + " breaks.";
       currentWeaponIndex--;
    }
 }
