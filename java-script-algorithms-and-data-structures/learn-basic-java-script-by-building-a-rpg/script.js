01f1091343f160f2add478874738a6ae56e29ec7
Evhen87 <kushner.aleks@yandex.ru>
2025-03-25 19:21:11 +0300

    Step 128

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -719,3 +719,7 @@ function attack() {
 function dodge() {
 
 }
+/*Step 128
+At the end of your code, create the defeatMonster and lose functions. Leave them empty for now.*/
+function defeatMonster() { }
+function lose() { }
