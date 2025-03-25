53dac1a0db7d3b0911d06150e9f310feebd59fbf
Evhen87 <kushner.aleks@yandex.ru>
2025-03-25 23:14:13 +0300

    Step 160

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -924,6 +924,18 @@ function easterEgg() {
 }
 /*Step 159
 Create an empty pick function with a parameter named guess.*/
+/*Step 160
+Create two new functions named pickTwo and pickEight.
+
+Inside each of those, call the pick() function and pass either 2 or 8 as the argument depending on the function name.*/
+function pickTwo() {
+   pick(2)
+}
+function pickEight() {
+   pick(8)
+}
+
+
 function pick(guess) {
 
 }
