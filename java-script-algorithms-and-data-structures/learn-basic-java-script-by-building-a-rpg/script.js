e8654e08668edac40ccd796e3b9856eb6c1d3d80
Evhen87 <kushner.aleks@yandex.ru>
2025-03-25 22:28:02 +0300

    Step 149

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -796,10 +796,28 @@ function add(num1, num2) {
   return num1 + num2;
 }
 Use the return keyword to return the value of hit at the end of the function.*/
+/*Step 149
+If you play the game in its current state you might notice a bug. If your xp is high enough, the getMonsterAttackValue function will return a negative number, which will actually add to your total health when fighting a monster! You can fix this issue by using a ternary operator to ensure negative values are not returned.
+
+The ternary operator is a conditional operator and can be used as a one-line if-else statement. The syntax is: condition ? expressionIfTrue : expressionIfFalse.
+
+Here is an example of returning a value using an if-else statement and a refactored example using a ternary operator:
+
+Example Code
+// if-else statement
+if (score > 0) {
+  return score
+} else {
+  return default_score
+}
+
+// ternary operator
+return score > 0 ? score : default_score
+In getMonsterAttackValue, change return hit to a ternary operator that returns hit if hit is greater than 0, or returns 0 if it is not.*/
 function getMonsterAttackValue(level) {
    const hit = (level * 5) - (Math.floor(Math.random() * xp));
    console.log(hit);
-   return hit;
+   return hit > 0 ? hit : 0;
 }
 function dodge() {
    text.innerText = "You dodge the attack from the " + monsters[fighting].name
