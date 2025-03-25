ba125362005f821ba78d9fb2f3fe56af9886ab9f
Evhen87 <kushner.aleks@yandex.ru>
2025-03-25 23:18:54 +0300

    Step 162

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -942,8 +942,8 @@ function pickTwo() {
 function pickEight() {
    pick(8)
 }
-
-
+/*Step 162
+Inside pick, use const to initialize a variable named numbers and set it to an empty array.*/
 function pick(guess) {
-
+   const numbers = [];
 }
