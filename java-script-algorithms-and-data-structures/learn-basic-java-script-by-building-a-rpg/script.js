43abf718b0c24fa75e43e0225ccf497ca00f71d7
Evhen87 <kushner.aleks@yandex.ru>
2025-03-25 23:28:46 +0300

    Step 164

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -953,9 +953,11 @@ Example Code
 while (condition) {
   // code to run
 }*/
+/*Step 164
+Inside your while loop, push a random number between 0 and 10 to the end of the numbers array. You can create this random number with Math.floor(Math.random() * 11).*/
 function pick(guess) {
    const numbers = [];
    while (numbers.length < 10) {
-
-   };
+      numbers.push(Math.floor(Math.random() * 11))
+   }
 }
