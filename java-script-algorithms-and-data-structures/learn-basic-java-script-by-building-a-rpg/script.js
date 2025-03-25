f81cbe82b506680a45d4bbea9bd0d63514d76469
Evhen87 <kushner.aleks@yandex.ru>
2025-03-25 19:17:47 +0300

    Step 126

--- java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
+++ java-script-algorithms-and-data-structures/learn-basic-java-script-by-building-a-rpg/script.js
@@ -691,6 +691,8 @@ Using these, you can generate a random number within a range. For example, this 
 Following this pattern, use the addition operator (+) to add a random number between 1 and the value of xp to your monsterHealth -= weapons[currentWeaponIndex].power.*/
 /*Step 125
 Update healthText.innerText and monsterHealthText.innerText to equal health and monsterHealth.*/
+/*Step 126
+Add an if statement to check if health is less than or equal to 0. If it is, call the lose function.*/
 function attack() {
    text.innerText = "The " + monsters[fighting].name + " attacks.";
    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
@@ -698,6 +700,9 @@ function attack() {
    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
    healthText.innerText = health;
    monsterHealthText.innerText = monsterHealth;
+   if (health <= 0) {
+      lose()
+   }
 }
 function dodge() {
 
