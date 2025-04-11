3e15300c759662feb97dd925fd5a5e52a73349df
Evhen87 <kushner.aleks@yandex.ru>
2025-04-11 22:48:30 +0300

    Step 35

--- java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
+++ java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
@@ -116,3 +116,10 @@ function isInvalidInput(str) {
    const regex = /\d+e\d+/i;
    return str.match(regex);
 }
+/*Step 35
+Now it is time to test your isInvalidInput function. For this test, you want to check if the function can detect scientific notation like 1e3 or 10e2. While this is a valid way to represent numbers, it is not a valid input for your calorie counter project.
+
+Below your isInvalidInput function, add a console statement. Inside that console statement, call the isInvalidInput function with an argument of "1e3".
+
+Open up the console to see the result. In the next step, you will learn more about what that result means.*/
+console.log(isInvalidInput("1e3"));
