fc44453a28654f4c7cdbb0bd5df6e2e0f4b9a9a0
Evhen87 <kushner.aleks@yandex.ru>
2025-04-11 21:34:00 +0300

    Step 17

--- java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
+++ java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
@@ -12,9 +12,21 @@ Begin by getting the form element (using the id) and storing it in a variable ca
 Get your #budget element and assign it to budgetNumberInput, and your #entry-dropdown element and assign it to entryDropdown.*/
 /*Step 16
 Following the same pattern, assign your #add-entry element to addEntryButton, your #clear element to clearButton, and your #output element to output.*/
+/*Step 17
+In programming, prefixing a variable with is or has is a common practice to signify that the variable represents a boolean value.
+
+Here are a few examples:
+
+Example Code
+let isRunning = true;
+let hasCompleted = false;
+Declare a variable named isError using let and initialize it with false, allowing for its reassignment later.
+
+Later on in the project, you will update the value of isError if the user provides an invalid input.*/
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+let isError = false; 
