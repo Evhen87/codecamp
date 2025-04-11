a295d0759e381fc32860319343cbf7ee9eb1f7e7
Evhen87 <kushner.aleks@yandex.ru>
2025-04-11 22:46:02 +0300

    Step 34

--- java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
+++ java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
@@ -104,6 +104,15 @@ Add this character class before and after e in your pattern.*/
 The + modifier in a regex allows you to match a pattern that occurs one or more times. To match your digit pattern one or more times, add a plus after each of the digit character classes. For example: [0-9]+.*/
 /*Step 33
 There is a shorthand character class to match any digit: \d. Replace your [0-9] character classes with this shorthand.*/
+/*Step 34
+Strings have a .match() method, which takes a regex argument. .match() will return an array of match results – containing either the first match, or all matches if the global flag is used.
+
+Example Code
+const str = 'example string';
+const regex = /example/;
+const result = str.match(regex); // Returns ['example']
+Return the result of calling the .match() method on str and passing your regex variable as the argument. You'll use this match result later on.*/
 function isInvalidInput(str) {
    const regex = /\d+e\d+/i;
+   return str.match(regex);
 }
