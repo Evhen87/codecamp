0b6589613b47d3d180d9f52f78bef15623160abb
Evhen87 <kushner.aleks@yandex.ru>
2025-04-11 21:54:16 +0300

    Step 24

--- java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
+++ java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
@@ -58,6 +58,17 @@ To tell the pattern to match each of these characters individually, you need to 
 Example Code
 const regex = /[helo]/;
 Turn your +-\s pattern into a character class. Note that you no longer need to escape the + character, because you are using a character class.*/
+/*Step 24
+JavaScript provides a .replace() method that enables you to replace characters in a string with another string. This method accepts two arguments. The first argument is the character sequence to be replaced, which can be either a string or a regex pattern. The second argument is the string that replaces the matched sequence.
+
+Since strings are immutable, the replace method returns a new string with the replaced characters.
+
+In this example, the replace method is used to replace all instances of the letter l with the number 1 in the string hello.
+
+Example Code
+"hello".replace(/l/g, "1");
+Use your regex to replace all instances of +, -, and a space in str with an empty string. Return this value.*/
 function cleanInputString(str) {
    const regex = /[+-\s]/g;
+   return str.replace(regex, "");
 }
