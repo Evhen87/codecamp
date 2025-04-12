e15ea8c5dfc822a0ecb2199899b7aa998c210955
Evhen87 <kushner.aleks@yandex.ru>
2025-04-12 19:43:34 +0300

    Step 64

--- java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
+++ java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
@@ -291,13 +291,17 @@ if (helloWorld) {
 
 }
 Add an if statement that checks if invalidInputMatch is truthy.*/
+/*Step 64
+Browsers have a built in alert() function, which you can use to display a pop-up message to the user. The message to display is passed as the argument to the alert() function.
+
+Using a template literal, in your if block, call the alert() function to tell the user "Invalid Input: ", followed by the first value in the invalidInputMatch array.*/
 function getCaloriesFromInputs(list) {
    let calories = 0;
    for (const item of list) {
       const currVal = cleanInputString(item.value);
       const invalidInputMatch = isInvalidInput(currVal);
       if (invalidInputMatch) {
-
+         alert(`Invalid Input: ${invalidInputMatch[0]}`)
       }
    };
 }
