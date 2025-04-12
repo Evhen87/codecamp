db9afbff17f5e77912a5f83510e9a57b0d5fa8bd
Evhen87 <kushner.aleks@yandex.ru>
2025-04-12 21:50:07 +0300

    Step 71

--- java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
+++ java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
@@ -259,10 +259,13 @@ Add a line to your calculateCalories function that calls the preventDefault() me
 Your function needs to get the values from the entries the user has added.
 
 Declare a breakfastNumberInputs variable, and give it the value of calling document.querySelectorAll() with the selector #breakfast input[type='number']. This will return any number inputs that are in the #breakfast element.*/
+/*Step 71
+Using that same syntax, query your number inputs in the #lunch element and assign them to lunchNumberInputs.*/
 function calculateCalories(e) {
    e.preventDefault();
    isError = false;
    const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
+   const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
 }
 /*Step 57
 Great! Now you can add entries without losing your previous inputs.
