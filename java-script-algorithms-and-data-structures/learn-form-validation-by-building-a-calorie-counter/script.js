3729cc2ddb5e056be2785370ace8d5f37f7dedcb
Evhen87 <kushner.aleks@yandex.ru>
2025-04-12 18:10:35 +0300

    Step 50

--- java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
+++ java-script-algorithms-and-data-structures/learn-form-validation-by-building-a-calorie-counter/script.js
@@ -198,11 +198,14 @@ Inside your template literal, create a label element and give it the text Entry 
 Give your label element a for attribute with the value X-#-name, where X is the value of the entryDropdown element and # is the value of entryNumber. Remember that HTML attributes should be wrapped in double quotes.*/
 /*Step 49
 After your label element, and on a new line in your template string, create an input element. Give it a type attribute set to text, a placeholder attribute set to Name, and an id attribute that matches the for attribute of your label element.*/
+/*Step 50
+Create another label element (on a new line) at the end of your HTMLString. This label should have the text Entry # Calories, using your template literal syntax to replace # with the value of entryNumber, and the for attribute set to X-#-calories, where X is the value of entryDropdown and # is the value of entryNumber.*/
 function addEntry() {
    function addEntry() {
       const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
    }
    const entryNumber = targetInputContainer.querySelectorAll().length;
    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" >`;
+   <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" >
+   <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>`;
 }
